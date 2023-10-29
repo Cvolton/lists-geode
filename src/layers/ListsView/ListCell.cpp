37f5f2f8bb14b5af9c0c542adac274c650c1ad57
Cvolton <cvolton@cvolton.eu>
2023-10-29 13:18:09 +0100

    remove unused code in listcell

--- src/layers/ListsView/ListCell.cpp
+++ src/layers/ListsView/ListCell.cpp
@@ -37,19 +37,6 @@ void ListCell::loadFromWrapper(CCNode* wrapper) {
     featureSprite->setScale(0.8f);
     this->m_mainLayer->addChild(featureSprite);
     featureSprite->setVisible(m_list.m_featuredIdx > 0);
-    //infoSprite->setScale(0.7f);
-
-    /*auto starsLabel = CCLabelBMFont::create(std::to_string(level->m_stars).c_str(), "bigFont.fnt");
-    starsLabel->setPosition({22, 8});
-    starsLabel->setScale(.34f);
-    starsLabel->setAnchorPoint({1, 0.5f});
-    this->m_mainLayer->addChild(starsLabel);
-
-    auto starsSprite = CCSprite::createWithSpriteFrameName("star_small01_001.png");
-    starsSprite->setPosition({23, 8});
-    starsSprite->setAnchorPoint({0, 0.5f});
-    starsSprite->setScale(0.8f);
-    this->m_mainLayer->addChild(starsSprite);*/
 
     auto title = CCLabelBMFont::create(m_list.m_name.c_str(), "bigFont.fnt");
     title->setAnchorPoint({ 0.0f, .5f });
@@ -118,107 +105,6 @@ void ListCell::loadFromWrapper(CCNode* wrapper) {
     diamond->setPosition(diamondSprite->getPositionX() + 13.f, 10.0f);
     diamond->setScale(.325f);
     this->m_mainLayer->addChild(diamond);
-
-    /*auto orbSprite = CCSprite::createWithSpriteFrameName("currencyOrbIcon_001.png");
-    orbSprite->setPosition({diamond->getPositionX() + ((diamond->getContentSize().width) * diamond->getScaleX()) + 5, 9});
-    orbSprite->setAnchorPoint({0, 0.5f});
-    orbSprite->setScale(0.5f);
-    this->m_mainLayer->addChild(orbSprite);
-
-    int orbsMax = (GSM->getAwardedCurrencyForLevel(level) * 125) / 100;
-    int orbsCollectible = GSM->getBaseCurrencyForLevel(level);
-    auto orb = CCLabelBMFont::create(CCString::createWithFormat("%i/%i", orbsCollectible, orbsMax)->getCString(), "bigFont.fnt");
-    if(orbsCollectible == orbsMax){ 
-        orb = CCLabelBMFont::create(CCString::createWithFormat("%i", orbsCollectible)->getCString(), "bigFont.fnt");
-        orb->setColor({100, 255, 255});
-    }
-    orb->setAnchorPoint({ 0.0f, 0.5f });
-    orb->setPosition(orbSprite->getPositionX() + 11.5f, 10.0f);
-    orb->setScale(.325f);
-    this->m_mainLayer->addChild(orb);
-
-    //row 1
-    auto percentSprite = CCSprite::createWithSpriteFrameName("GJ_arrow_01_001.png");
-    percentSprite->setFlipX(true);
-    percentSprite->setPosition({44, 23});
-    percentSprite->setAnchorPoint({0, 0.5f});
-    percentSprite->setScale(0.35f);
-    this->m_mainLayer->addChild(percentSprite);
-
-    auto percent = CCLabelBMFont::create(CCString::createWithFormat("%i%%", level->m_normalPercent.value())->getCString(), "bigFont.fnt");
-    percent->setAnchorPoint({ 0.0f, 0.5f });
-    percent->setPosition(57.5f, 24.0f);
-    percent->setScale(.325f);
-    if(level->m_normalPercent == 100) percent->setColor({255,255,128});
-    this->m_mainLayer->addChild(percent);
-
-    auto practiceSprite = CCSprite::createWithSpriteFrameName("checkpoint_01_001.png");
-    practiceSprite->setPosition({percent->getPositionX() + ((percent->getContentSize().width) * percent->getScaleX()) + 5, 23});
-    practiceSprite->setAnchorPoint({0, 0.5f});
-    practiceSprite->setScale(0.35f);
-    this->m_mainLayer->addChild(practiceSprite);
-
-    auto practice = CCLabelBMFont::create(CCString::createWithFormat("%i%%", level->m_practicePercent)->getCString(), "bigFont.fnt");
-    practice->setAnchorPoint({ 0.0f, 0.5f });
-    practice->setPosition(practiceSprite->getPositionX() + 8.f, 24.0f);
-    practice->setScale(.325f);
-    if(level->m_practicePercent == 100) practice->setColor({255,255,128});
-    this->m_mainLayer->addChild(practice);
-
-    auto coinDict = GameStatsManager::sharedState()->m_verifiedUserCoins;
-
-    float coinX = practice->getPositionX() + ((practice->getContentSize().width) * practice->getScaleX());
-    for(int i = 1; i <= biCache->getCoinCount(level->m_levelID); i++){
-        bool isCollected = coinDict->objectForKey(level->getCoinKey(i)) == nullptr;
-        auto coinSprite = CCSprite::createWithSpriteFrameName("usercoin_small01_001.png");
-        if(isCollected) coinSprite->setColor({165, 165, 165});
-        coinSprite->setPosition({coinX += 6, 23});
-        coinSprite->setAnchorPoint({0, 0.5f});
-        coinSprite->setScale(0.7f);
-        //coinSprite->setScale(0.35f);
-        this->m_mainLayer->addChild(coinSprite);
-    }
-
-    //fix level data (deleted from saved)
-    level->m_levelName = biCache->getLevelName(level->m_levelID);
-    level->m_coins = biCache->getCoinCount(level->m_levelID);
-    level->m_coinsVerified = true;
-
-    //buttons
-    auto menu = CCMenu::create();
-
-    auto viewSprite = CCSprite::createWithSpriteFrameName("GJ_playBtn2_001.png");
-    viewSprite->setScale(.5f);
-    auto viewBtn = CCMenuItemSpriteExtra::create(
-        viewSprite,
-        this,
-        menu_selector(ListCell::onView)
-    );
-    //viewBtn->setPosition({42, -132});
-    viewBtn->setPosition(CCPoint(327, 28) - (winSize / 2));
-    menu->addChild(viewBtn);
-
-    auto leaderboardSprite = CCSprite::createWithSpriteFrameName("GJ_levelLeaderboardBtn_001.png");
-    leaderboardSprite->setScale(.85f);
-    auto leaderboardBtn = CCMenuItemSpriteExtra::create(
-        leaderboardSprite,
-        this,
-        menu_selector(ListCell::onLeaderboards)
-    );
-    leaderboardBtn->setPosition(viewBtn->getPosition() - CCPoint(40, 0));
-    menu->addChild(leaderboardBtn);
-
-    auto infoSprite = CCSprite::createWithSpriteFrameName("GJ_infoBtn_001.png");
-    infoSprite->setScale(.85f);
-    auto infoBtn = CCMenuItemSpriteExtra::create(
-        infoSprite,
-        this,
-        menu_selector(ListCell::onInfo)
-    );
-    infoBtn->setPosition(leaderboardBtn->getPosition() - CCPoint(40, 0));
-    menu->addChild(infoBtn);
-
-    this->m_mainLayer->addChild(menu);*/
     auto menu = CCMenu::create();
     auto similarButton = createButton(menu, "View", menu_selector(ListCell::onView), 28, -132, 40.f, 25.f, 0.5f);
     m_mainLayer->addChild(menu);
