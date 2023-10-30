4f65f9da84c69ee3c9950f8021eb78a7b512a565
Cvolton <cvolton@cvolton.eu>
2023-10-31 00:31:21 +0100

    add lists button to profilepage

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,10 @@
 #include <Geode/Geode.hpp>
 #include <Geode/modify/CreatorLayer.hpp>
+#include <Geode/modify/ProfilePage.hpp>
 
 #include "layers/ListsSearchLayer.h"
+#include "layers/ListsViewLayer.h"
+#include "objects/ListSearchObject.hpp"
 
 using namespace geode::prelude;
 
@@ -35,5 +38,42 @@ class $modify(ListsCreatorLayer, CreatorLayer) {
         menu->updateLayout();
 
 		return true;
+	}
+};
+
+class $modify(ListsProfilePage, ProfilePage) {
+	void onLists(CCObject*) {
+		ListSearchObject obj;
+		obj.m_type = 5;
+		obj.m_str = std::to_string(m_accountID);
+
+		auto browserLayer = ListsViewLayer::scene(obj);
+		auto transitionFade = CCTransitionFade::create(0.5, browserLayer);
+		CCDirector::sharedDirector()->pushScene(transitionFade);
+	}
+
+	void loadPageFromUserInfo(GJUserScore* score) {
+		ProfilePage::loadPageFromUserInfo(score);
+
+		auto listsBtnSpr = AccountButtonSprite::createWithSpriteFrameName("geode.loader/changelog.png", 1.f);
+		listsBtnSpr->setScale(.75f);
+
+		auto listsBtn = CCMenuItemSpriteExtra::create(
+            listsBtnSpr, this, menu_selector(ListsProfilePage::onLists)
+        );
+		m_buttonMenu->addChild(listsBtn);
+		this->m_buttons->addObject(listsBtn);
+
+		if(Mod::get()->getSettingValue<bool>("alt-profilepage")) listsBtn->setPosition(408, -224);
+		else {
+			listsBtn->setPosition(368, -258);
+
+			//hide my levels text
+			auto winSize = CCDirector::sharedDirector()->getWinSize();
+			for(unsigned int i = 0; i < m_mainLayer->getChildrenCount(); i++){
+				CCNode* node = dynamic_cast<CCNode*>(m_mainLayer->getChildren()->objectAtIndex(i));
+				if(node != nullptr && node->getPositionX() == (winSize.width / 2) + 138 && node->getPositionY() == (winSize.height / 2) - 123) node->setVisible(false);
+			}
+		}
 	}
 };
